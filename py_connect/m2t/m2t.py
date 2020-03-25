081077dccec0c641393c17e574e0fc1ab20883bf
Iasonas Paraskevopoulos <iaswnparaskev@gmail.com>
2020-03-25 20:41:04 +0200

    Initial m2t for simple sensor

--- py_connect/m2t/m2t.py
+++ py_connect/m2t/m2t.py
@@ -3,12 +3,41 @@
 The m2t enginge for producing code for handling the code generation.
 """
 
+import os
+import autopep8
 from jinja2 import Environment, FileSystemLoader
+from ..hw_devices import *
 
 
-def generate():
+def generate(comp_device):
     """Generate code"""
-    file_loader = FileSystemLoader("templates")
+    path = "/".join(os.path.dirname(os.path.abspath(__file__)).split("/")[:])
+
+    file_loader = FileSystemLoader(path + "/templates")
     env = Environment(loader=file_loader)
 
-    import_tmpl = env.get_template("pidevices_import.py.tmpl")
+    # Iterate through all connected devices
+    for dev in comp_device.devices:
+        if dev.type == DeviceType.SENSOR:
+            tmpl = env.get_template("pidevices_sensor.py.tmpl")
+            
+            # Dictionary with the gpio pins
+            gpio_pins = {}
+            i2c_bus = None
+            spi_bus = None
+
+            # Get information from pins.
+            for pin in dev.pins:
+
+                # GPIO Pins
+                if isinstance(pin, Input):
+                    gpio_pins[pin.conn_from.name] = pin.conn_from.number
+
+                if isinstance(pin, Output):
+                    gpio_pins[pin.conn_to.name] = pin.conn_to.number
+
+        # Generate code for device
+        output = tmpl.render(device_class=dev.name, args=gpio_pins)
+        output = autopep8.fix_code(output)
+        print(output)
+
