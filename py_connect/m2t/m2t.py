c6df7e2cfa2a6f2fd3d408ee25a9832fc8d06981
Iasonas Paraskevopoulos <iaswnparaskev@gmail.com>
2020-03-26 23:49:43 +0200

    .

--- py_connect/m2t/m2t.py
+++ py_connect/m2t/m2t.py
@@ -9,35 +9,63 @@ from jinja2 import Environment, FileSystemLoader
 from ..hw_devices import *
 
 
-def generate(comp_device):
+class Generator():
     """Generate code"""
-    path = "/".join(os.path.dirname(os.path.abspath(__file__)).split("/")[:])
 
-    file_loader = FileSystemLoader(path + "/templates")
-    env = Environment(loader=file_loader)
+    def __init__(self):
+        """Construct the generator."""
 
-    # Iterate through all connected devices
-    for dev in comp_device.devices:
-        if dev.type == DeviceType.SENSOR:
-            tmpl = env.get_template("pidevices_sensor.py.tmpl")
-            
-            # Dictionary with the gpio pins
-            gpio_pins = {}
-            i2c_bus = None
-            spi_bus = None
+        path = os.path.dirname(os.path.abspath(__file__))
+        # Load templates
+        file_loader = FileSystemLoader(path + "/templates")
+        self.env = Environment(loader=file_loader)
 
-            # Get information from pins.
-            for pin in dev.pins:
+    def generate(self, comp_device):
+        """Genearate code from a device.
 
-                # GPIO Pins
-                if isinstance(pin, Input):
-                    gpio_pins[pin.conn_from.name] = pin.conn_from.number
+        Args:
+            comp_device (Device object): A computational device object. That 
+                has n connected devices.
 
-                if isinstance(pin, Output):
-                    gpio_pins[pin.conn_to.name] = pin.conn_to.number
+        Returns:
+            (str): A string that contains the generated code.
+        """
+        # TODO: Use different template regarding the type of the board
+        # For example raspberry pi, riot ...
+        tmpl_type = "pidevices"
 
-        # Generate code for device
-        output = tmpl.render(device_class=dev.name, args=gpio_pins)
-        output = autopep8.fix_code(output)
-        print(output)
+        # Iterate through all connected devices
+        for dev in comp_device.devices:
+            if dev.type == DeviceType.SENSOR:
+                tmpl = self.env.get_template(tmpl_type + "_sensor.py.tmpl")
+                args = {}  # Arguments for the driver constructor
 
+                if isinstance(dev, GpioDevice):
+
+                    # Get information from pins.
+                    for pin in dev.gpio_pins:
+                        if isinstance(pin, GpioInput):
+                            args[pin.conn_from.name] = pin.conn_from.number
+
+                        if isinstance(pin, GpioOutput):
+                            args[pin.conn_to.name] = pin.conn_to.number
+
+                # handle i2c devices
+                if isinstance(dev, I2cDevice):
+                    args['bus'] = dev.i2c_bus
+
+            # Generate code for device
+            output = tmpl.render(device_class=dev.name, args=gpio_pins)
+            output = autopep8.fix_code(output)
+
+        return output
+
+    def _handle_gpio(self, device):
+        """_handle_gpio
+
+        Args:
+            device ():
+
+        Returns:
+        """
+        pass
--- tests/test_generation.py
+++ tests/test_generation.py
@@ -2,26 +2,35 @@ import sys
 sys.path.append(".")
 
 from py_connect.hw_devices import *  # noqa E402
-from py_connect.m2t import m2t  # noqa E402
+from py_connect.m2t.m2t import Generator  # noqa E402
 
 
 # Create a raspberry pi
 pi = Computational(operating_voltage=3.3)
-pi_power = Power5V(number=2, name="pi_5v")
-pi_gnd = Gnd(number=6, name="pi_gnd")
-pi_echo = Input(number=14, name="echo_pin")
-pi_trigger = Output(number=15, name="trigger_pin")
-pi.pins.extend([pi_power, pi_gnd, pi_echo, pi_trigger])
+pi_power = Power5V(name="pi_5v")
+pi_gnd = Gnd(name="pi_gnd")
+pi_echo = GpioInput(number=14, name="echo_pin")
+pi_trigger = GpioOutput(number=15, name="trigger_pin")
+pi.power_pins.extend([pi_power, pi_gnd])
+pi.pins.extend([pi_echo, pi_trigger])
 
 # Create a sonar
-sonar = NonComputational(operating_voltage=5.0, type=DeviceType.SENSOR)
-sonar.name = "HcSr04RPiGPIO"
-sonar_power = Power5V(number=1, name="sonar_5v")
-sonar_gnd = Gnd(number=2, name="sonar_gnd")
-sonar_echo = Output(number=3, name="echo")
-sonar_trigger = Input(number=4, name="trigger")
-sonar.pins.extend([sonar_power, sonar_gnd, sonar_echo, sonar_trigger])
+sonar = GpioDevice(operating_voltage=5.0, type=DeviceType.SENSOR)
+sonar.driver = "HcSr04RPiGPIO"
+sonar_power = Power5V(name="sonar_5v")
+sonar_gnd = Gnd(name="sonar_gnd")
+sonar_echo = GpioOutput(number=3, name="echo")
+sonar_trigger = GpioInput(number=4, name="trigger")
+sonar.power_pins.extend([sonar_power, sonar_gnd])
+sonar.gpio_pins.extend([sonar_echo, sonar_trigger])
 
+# Create vl53l1x
+tof = I2cDevice(operating_voltage=3.3, type=DeviceType.SENSOR)
+tof.driver = "VL53L1X"
+sonar_power = Power5V(name="sonar_5v")
+sonar_gnd = Gnd(name="sonar_gnd")
+sonar_echo = GpioOutput(number=3, name="echo")
+sonar_trigger = GpioInput(number=4, name="trigger")
 # Make a connection
 pi.devices.append(sonar)
 pi_power.conn_to.append(sonar_power)
@@ -29,4 +38,7 @@ pi_gnd.conn_to.append(sonar_gnd)
 pi_echo.conn_from = sonar_echo
 pi_trigger.conn_to = sonar_trigger
 
-m2t.generate(pi)
+# Create validator
+gen = Generator()
+
+print(gen.generate(pi))
