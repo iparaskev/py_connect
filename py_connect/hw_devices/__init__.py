048f84796698b0f0b2e9a320cc5bbdef33e5753b
Iasonas Paraskevopoulos <iaswnparaskev@gmail.com>
2020-03-27 16:43:47 +0200

    Add device general model

--- py_connect/hw_devices/__init__.py
+++ py_connect/hw_devices/__init__.py
@@ -1,47 +1,23 @@
 
 from .hw_devices import getEClassifier, eClassifiers
 from .hw_devices import name, nsURI, nsPrefix, eClass
-from .hw_devices import Device, Pin, PowerPin, IOPin, Power3V3, Power5V, Gnd, GpioInput, GpioOutput, I2cSda, I2cScl, Pwm, UartRx, UartTx, SpiMiso, SpiSclk, SpiMosi, SpiCe, Computational, NonComputational, DeviceType, GpioDevice, I2cDevice, CompositeDevice, GpioPin, I2cPin, SpiPin, UartPin
+from .hw_devices import Device, Computational, NonComputational, DeviceType, Pin, PowerPin, IOPinFunction, IOPin, IOType, PowerType
 
 
 from . import hw_devices
 
-__all__ = ['Device', 'Pin', 'PowerPin', 'IOPin', 'Power3V3', 'Power5V', 'Gnd', 'GpioInput', 'GpioOutput', 'I2cSda', 'I2cScl', 'Pwm', 'UartRx', 'UartTx', 'SpiMiso',
-           'SpiSclk', 'SpiMosi', 'SpiCe', 'Computational', 'NonComputational', 'DeviceType', 'GpioDevice', 'I2cDevice', 'CompositeDevice', 'GpioPin', 'I2cPin', 'SpiPin', 'UartPin']
+__all__ = ['Device', 'Computational', 'NonComputational', 'DeviceType',
+           'Pin', 'PowerPin', 'IOPinFunction', 'IOPin', 'IOType', 'PowerType']
 
 eSubpackages = []
 eSuperPackage = None
 hw_devices.eSubpackages = eSubpackages
 hw_devices.eSuperPackage = eSuperPackage
 
-Device.power_pins.eType = PowerPin
-Computational.pins.eType = Pin
-Computational.devices.eType = NonComputational
-GpioDevice.gpio_pins.eType = GpioPin
-I2cDevice.i2c_pins.eType = I2cPin
-Power3V3.conn_to.eType = Power3V3
-Power3V3.conn_from.eType = Power3V3
-Power3V3.conn_from.eOpposite = Power3V3.conn_to
-Power5V.conn_to.eType = Power5V
-Power5V.conn_from.eType = Power5V
-Power5V.conn_from.eOpposite = Power5V.conn_to
-Gnd.conn_to.eType = Gnd
-Gnd.conn_from.eType = Gnd
-Gnd.conn_from.eOpposite = Gnd.conn_to
-GpioInput.conn_from.eType = GpioOutput
-GpioOutput.conn_to.eType = GpioInput
-GpioOutput.conn_to.eOpposite = GpioInput.conn_from
-I2cSda.conn_to.eType = I2cSda
-I2cSda.conn_from.eType = I2cSda
-I2cSda.conn_from.eOpposite = I2cSda.conn_to
-I2cScl.conn_to.eType = I2cScl
-I2cScl.conn_from.eType = I2cScl
-I2cScl.conn_from.eOpposite = I2cScl.conn_to
-Pwm.conn_to.eType = Pwm
-Pwm.conn_from.eType = Pwm
-Pwm.conn_from.eOpposite = Pwm.conn_to
+Device.pins.eType = Pin
+IOPin.functions.eType = IOPinFunction
 
-otherClassifiers = [DeviceType]
+otherClassifiers = [DeviceType, IOType, PowerType]
 
 for classif in otherClassifiers:
     eClassifiers[classif.name] = classif
--- py_connect/hw_devices/hw_devices.py
+++ py_connect/hw_devices/hw_devices.py
@@ -14,14 +14,19 @@ eClassifiers = {}
 getEClassifier = partial(Ecore.getEClassifier, searchspace=eClassifiers)
 DeviceType = EEnum('DeviceType', literals=['SENSOR', 'ACTUATOR'])
 
+IOType = EEnum('IOType', literals=['GPIO_INPUT', 'GPIO_OUTPUT',
+                                   'GPIO_BOTH', 'I2C_SDA', 'I2C_SCL', 'PWM'])
+
+PowerType = EEnum('PowerType', literals=['GND', 'POWER_3V3', 'POWER_5V'])
+
 
 class Device(EObject, metaclass=MetaEClass):
 
     operating_voltage = EAttribute(eType=EFloat, derived=False, changeable=True)
     name = EAttribute(eType=EString, derived=False, changeable=True)
-    power_pins = EReference(ordered=True, unique=True, containment=False, upper=-1)
+    pins = EReference(ordered=True, unique=True, containment=False, upper=-1)
 
-    def __init__(self, *, operating_voltage=None, name=None, power_pins=None, **kwargs):
+    def __init__(self, *, operating_voltage=None, name=None, pins=None, **kwargs):
         if kwargs:
             raise AttributeError('unexpected arguments: {}'.format(kwargs))
 
@@ -33,312 +38,84 @@ class Device(EObject, metaclass=MetaEClass):
         if name is not None:
             self.name = name
 
-        if power_pins:
-            self.power_pins.extend(power_pins)
+        if pins:
+            self.pins.extend(pins)
 
 
 class Pin(EObject, metaclass=MetaEClass):
 
-    name = EAttribute(eType=EString, derived=False, changeable=True)
-
-    def __init__(self, *, name=None, **kwargs):
-        if kwargs:
-            raise AttributeError('unexpected arguments: {}'.format(kwargs))
-
-        super().__init__()
-
-        if name is not None:
-            self.name = name
-
-
-class UartRx(EObject, metaclass=MetaEClass):
-
-    def __init__(self, **kwargs):
-        if kwargs:
-            raise AttributeError('unexpected arguments: {}'.format(kwargs))
-
-        super().__init__()
-
-
-class UartTx(EObject, metaclass=MetaEClass):
-
-    def __init__(self, **kwargs):
-        if kwargs:
-            raise AttributeError('unexpected arguments: {}'.format(kwargs))
-
-        super().__init__()
-
-
-class SpiMiso(EObject, metaclass=MetaEClass):
-
-    def __init__(self, **kwargs):
-        if kwargs:
-            raise AttributeError('unexpected arguments: {}'.format(kwargs))
-
-        super().__init__()
-
-
-class SpiSclk(EObject, metaclass=MetaEClass):
-
-    def __init__(self, **kwargs):
-        if kwargs:
-            raise AttributeError('unexpected arguments: {}'.format(kwargs))
-
-        super().__init__()
-
-
-class SpiMosi(EObject, metaclass=MetaEClass):
-
-    def __init__(self, **kwargs):
-        if kwargs:
-            raise AttributeError('unexpected arguments: {}'.format(kwargs))
-
-        super().__init__()
-
-
-class SpiCe(EObject, metaclass=MetaEClass):
-
-    def __init__(self, **kwargs):
-        if kwargs:
-            raise AttributeError('unexpected arguments: {}'.format(kwargs))
-
-        super().__init__()
-
-
-class SpiPin(EObject, metaclass=MetaEClass):
-
-    def __init__(self, **kwargs):
-        if kwargs:
-            raise AttributeError('unexpected arguments: {}'.format(kwargs))
-
-        super().__init__()
-
-
-class UartPin(EObject, metaclass=MetaEClass):
-
-    def __init__(self, **kwargs):
-        if kwargs:
-            raise AttributeError('unexpected arguments: {}'.format(kwargs))
-
-        super().__init__()
-
-
-class PowerPin(Pin):
-
-    def __init__(self, **kwargs):
-
-        super().__init__(**kwargs)
-
-
-class IOPin(Pin):
-
     number = EAttribute(eType=EInt, derived=False, changeable=True)
 
     def __init__(self, *, number=None, **kwargs):
+        if kwargs:
+            raise AttributeError('unexpected arguments: {}'.format(kwargs))
 
-        super().__init__(**kwargs)
+        super().__init__()
 
         if number is not None:
             self.number = number
 
 
+class IOPinFunction(EObject, metaclass=MetaEClass):
+
+    type = EAttribute(eType=IOType, derived=False, changeable=True)
+    hw_port = EAttribute(eType=EInt, derived=False, changeable=True, default_value=-1)
+
+    def __init__(self, *, type=None, hw_port=None, **kwargs):
+        if kwargs:
+            raise AttributeError('unexpected arguments: {}'.format(kwargs))
+
+        super().__init__()
+
+        if type is not None:
+            self.type = type
+
+        if hw_port is not None:
+            self.hw_port = hw_port
+
+
 class Computational(Device):
 
-    pins = EReference(ordered=True, unique=True, containment=False, upper=-1)
-    devices = EReference(ordered=True, unique=True, containment=False, upper=-1)
-
-    def __init__(self, *, pins=None, devices=None, **kwargs):
+    def __init__(self, **kwargs):
 
         super().__init__(**kwargs)
-
-        if pins:
-            self.pins.extend(pins)
-
-        if devices:
-            self.devices.extend(devices)
 
 
 class NonComputational(Device):
 
     type = EAttribute(eType=DeviceType, derived=False, changeable=True)
-    driver = EAttribute(eType=EString, derived=False, changeable=True)
 
-    def __init__(self, *, type=None, driver=None, **kwargs):
+    def __init__(self, *, type=None, **kwargs):
 
         super().__init__(**kwargs)
 
         if type is not None:
             self.type = type
 
-        if driver is not None:
-            self.driver = driver
 
+class PowerPin(Pin):
 
-class Power3V3(PowerPin):
+    function = EAttribute(eType=PowerType, derived=False, changeable=True)
 
-    conn_to = EReference(ordered=True, unique=True, containment=False, upper=-1)
-    conn_from = EReference(ordered=True, unique=True, containment=False)
-
-    def __init__(self, *, conn_to=None, conn_from=None, **kwargs):
+    def __init__(self, *, function=None, **kwargs):
 
         super().__init__(**kwargs)
 
-        if conn_to:
-            self.conn_to.extend(conn_to)
-
-        if conn_from is not None:
-            self.conn_from = conn_from
+        if function is not None:
+            self.function = function
 
 
-class Power5V(PowerPin):
+class IOPin(Pin):
 
-    conn_to = EReference(ordered=True, unique=True, containment=False, upper=-1)
-    conn_from = EReference(ordered=True, unique=True, containment=False)
+    name = EAttribute(eType=EString, derived=False, changeable=True)
+    functions = EReference(ordered=True, unique=True, containment=False, upper=-1)
 
-    def __init__(self, *, conn_to=None, conn_from=None, **kwargs):
+    def __init__(self, *, name=None, functions=None, **kwargs):
 
         super().__init__(**kwargs)
 
-        if conn_to:
-            self.conn_to.extend(conn_to)
-
-        if conn_from is not None:
-            self.conn_from = conn_from
-
-
-class Gnd(PowerPin):
-
-    conn_to = EReference(ordered=True, unique=True, containment=False, upper=-1)
-    conn_from = EReference(ordered=True, unique=True, containment=False, upper=-1)
-
-    def __init__(self, *, conn_to=None, conn_from=None, **kwargs):
-
-        super().__init__(**kwargs)
-
-        if conn_to:
-            self.conn_to.extend(conn_to)
-
-        if conn_from:
-            self.conn_from.extend(conn_from)
-
-
-class GpioDevice(NonComputational):
-
-    gpio_pins = EReference(ordered=True, unique=True, containment=False, upper=-1)
-
-    def __init__(self, *, gpio_pins=None, **kwargs):
-
-        super().__init__(**kwargs)
-
-        if gpio_pins:
-            self.gpio_pins.extend(gpio_pins)
-
-
-class I2cDevice(NonComputational):
-
-    i2c_pins = EReference(ordered=True, unique=True, containment=False, upper=-1)
-
-    def __init__(self, *, i2c_pins=None, **kwargs):
-
-        super().__init__(**kwargs)
-
-        if i2c_pins:
-            self.i2c_pins.extend(i2c_pins)
-
-
-class GpioPin(IOPin):
-
-    def __init__(self, **kwargs):
-
-        super().__init__(**kwargs)
-
-
-class I2cPin(IOPin):
-
-    i2c_bus = EAttribute(eType=EInt, derived=False, changeable=True)
-
-    def __init__(self, *, i2c_bus=None, **kwargs):
-
-        super().__init__(**kwargs)
-
-        if i2c_bus is not None:
-            self.i2c_bus = i2c_bus
-
-
-class GpioInput(GpioPin):
-
-    conn_from = EReference(ordered=True, unique=True, containment=False)
-
-    def __init__(self, *, conn_from=None, **kwargs):
-
-        super().__init__(**kwargs)
-
-        if conn_from is not None:
-            self.conn_from = conn_from
-
-
-class GpioOutput(GpioPin):
-
-    conn_to = EReference(ordered=True, unique=True, containment=False)
-
-    def __init__(self, *, conn_to=None, **kwargs):
-
-        super().__init__(**kwargs)
-
-        if conn_to is not None:
-            self.conn_to = conn_to
-
-
-class I2cSda(I2cPin):
-
-    conn_to = EReference(ordered=True, unique=True, containment=False, upper=-1)
-    conn_from = EReference(ordered=True, unique=True, containment=False, upper=-1)
-
-    def __init__(self, *, conn_to=None, conn_from=None, **kwargs):
-
-        super().__init__(**kwargs)
-
-        if conn_to:
-            self.conn_to.extend(conn_to)
-
-        if conn_from:
-            self.conn_from.extend(conn_from)
-
-
-class I2cScl(I2cPin):
-
-    conn_to = EReference(ordered=True, unique=True, containment=False, upper=-1)
-    conn_from = EReference(ordered=True, unique=True, containment=False, upper=-1)
-
-    def __init__(self, *, conn_to=None, conn_from=None, **kwargs):
-
-        super().__init__(**kwargs)
-
-        if conn_to:
-            self.conn_to.extend(conn_to)
-
-        if conn_from:
-            self.conn_from.extend(conn_from)
-
-
-class Pwm(GpioPin):
-
-    conn_to = EReference(ordered=True, unique=True, containment=False)
-    conn_from = EReference(ordered=True, unique=True, containment=False)
-
-    def __init__(self, *, conn_to=None, conn_from=None, **kwargs):
-
-        super().__init__(**kwargs)
-
-        if conn_to is not None:
-            self.conn_to = conn_to
-
-        if conn_from is not None:
-            self.conn_from = conn_from
-
-
-class CompositeDevice(GpioDevice, I2cDevice):
-
-    def __init__(self, **kwargs):
-
-        super().__init__(**kwargs)
+        if name is not None:
+            self.name = name
+
+        if functions:
+            self.functions.extend(functions)
