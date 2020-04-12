814c4ada769e08289ed0952d4098c7ac36c24aeb
Iasonas Paraskevopoulos <iaswnparaskev@gmail.com>
2020-04-12 19:47:18 +0300

    Add model loader function

--- py_connect/model_loader.py
+++ py_connect/model_loader.py
@@ -1,7 +1,10 @@
 """model_loader.py"""
 
 import os
+import importlib
 from pyecore.resources import ResourceSet, URI
+import sys
+sys.path.append(".")
 
 
 def load_model(name):
@@ -23,3 +26,21 @@ def load_model(name):
     # Load model instance
     r = rset.get_resource(URI(root_path + "/devices_db/" + name + ".xmi"))
     return r.contents[0]
+
+
+def load_model_py(name):
+    """Load a device model from a xmi.
+
+    Args:
+        name (str): Name of the device.
+
+    Returns:
+        (Device Object): A board or a peripheral device object.
+    """
+    f_path = os.path.abspath(__file__)
+    root_path = "/".join(os.path.dirname(f_path).split("/")[:-1])
+    module = importlib.import_module("devices_db." + name)
+    return getattr(module, "dev")
+
+
+#print(load_model_py("rpi_3b_plus"))
