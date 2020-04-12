b5fb307b111a45e83738236673214727dadaf662
Iasonas Paraskevopoulos <iaswnparaskev@gmail.com>
2020-04-12 20:46:26 +0300

    minor

--- devices_db/hc_sr04.py
+++ devices_db/hc_sr04.py
@@ -8,14 +8,14 @@ from py_connect.hw_devices import *  # noqa E402
 NAME = "devices_db/hc_sr04.xmi"  # Name of the xmi
 
 
-dev = Peripheral(vcc=5.0, 
-                 name="hc_sr04_sonar",
+dev = Peripheral(vcc=5.0,
+                 name="hc_sr04",
                  type=PeripheralType.SENSOR,
                  operating_voltage=5.,
                  digital_pins=2)
 pin_1 = Power5V(number=1)
-pin_2 = DigitalPin(name="trigger", number=2)
-pin_3 = DigitalPin(name="echo", number=3)
+pin_2 = DigitalPin(name="trigger_pin", number=2)
+pin_3 = DigitalPin(name="echo_pin", number=3)
 pin_4 = Gnd(number=4)
 
 gpio_1 = GPIO(pin=pin_2, type=GPIOType.INPUT)
