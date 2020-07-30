6fa6acbc01f827fcbfb5fb65c6453b52ec539005
Iasonas Paraskevopoulos <iaswnparaskev@gmail.com>
2020-07-31 00:18:56 +0300

    Spi schematic has error

--- py_connect/__init__.py
+++ py_connect/__init__.py
@@ -32,7 +32,11 @@ def parse_args():
     parser.add_argument("--connections",
                         help="Path to a connection specification.")
     parser.add_argument("--source",
-                        help="The name of the source code.")
+                        help="Flag for generating source code.",
+                        action="store_true")
+    parser.add_argument("--schematic",
+                        help="The name of the source code.",
+                        action="store_true")
     parser.add_argument("--specific_con",
                         help="The name of the specific connection.")
     parser.add_argument("--update_pidevices",
@@ -80,16 +84,30 @@ def main():  # noqa C901
                     conn_name = args.specific_con
                     source =\
                         m2t.generate(connections.connections[args.specific_con])
-                    m2t.write_source(source, args.source)
+                    m2t.write_source(source, conn_name + ".py")
                 else:
                     for key in connections.connections.keys():
                         conn_name = key
                         source = m2t.generate(connections.connections[key])
-                        m2t.write_source(source, key + "_" + args.source)
+                        m2t.write_source(source, conn_name + ".py")
             except NotImplementedDriverError as e:
                 print(e)
                 search_repos(connections.connections[conn_name].peripheral.name,
                              "python")
+
+        # Schematic image.
+        if args.schematic:
+            drawer = Drawer()
+            if args.specific_con:
+                conn_name = args.specific_con
+                drawer.draw_connection(connections.connections[args.specific_con])
+                drawer.save(conn_name + ".png")
+            else:
+                for key in connections.connections.keys():
+                    conn_name = key
+                    print(conn_name)
+                    drawer.draw_connection(connections.connections[key])
+                    drawer.save(conn_name + ".png")
 
     if args.update_pidevices:
         getter = ImplementationsGetter("pidevices")
